83585f4a115e0de57ff77abe26601112fc60636f
TiezhengYuan <tiezhengyuan@hotmail.com>
2023-04-24 10:04:33 -0400

    iterate list

---
+++ search/iterate_list.py
@@ -0,0 +1,72 @@
+
+
+import string
+
+class IterateList:
+
+    @staticmethod
+    def is_subset(arr1:list, arr2:list)->bool:
+        '''
+        check if one array is a subset of the other
+        '''
+        small, large = (arr1, arr2) if len(arr1) < len(arr2) \
+            else (arr2, arr1)
+        for i in small:
+            if i not in large:
+                return False
+        return True
+
+    @staticmethod
+    def get_intersection_1(arr1:list, arr2:list)->list:
+        '''
+        intersection of two lists
+        complexity: O(N1*N2)
+        '''
+        share = []
+        for i in arr1:
+            if i in arr2 and i not in share:
+                share.append(i)
+        return share
+
+    @staticmethod
+    def detect_first_duplicate(arr:list):
+        '''
+        dtect the first duplicate value
+        complexity: O(N)
+        '''
+        if len(arr) > 0:
+            pool = {}
+            for i,val in enumerate(arr):
+                if val in pool:
+                    return val
+                else:
+                    pool[val] = i
+        return None
+
+    @staticmethod
+    def detect_first_unique(arr:list):
+        '''
+        complexity: O(N)
+        '''
+        unique, duplicate = [], []
+        for val in arr:
+            if val not in duplicate:
+                if val in unique:
+                    unique.remove(val)
+                    duplicate.append(val)
+                else:
+                    unique.append(val)
+        return unique[0] if unique else None
+
+
+    @staticmethod
+    def detect_missing_letters(input:str):
+        '''
+        complexity: O(N)
+        '''
+        alphabet = {i:0 for i in string.ascii_lowercase}
+        for i in input:
+            if i not in (' ', ''):
+                alphabet[i] += 1
+        missing = [k for k,v in alphabet.items() if v==0]
+        return ''.join(missing)
---
+++ tests/search/test_iterate_list.py
@@ -0,0 +1,54 @@
+from tests.helper import *
+from search.iterate_list import IterateList as c
+
+@ddt
+class TestIterateList(TestCase):
+
+    @data(
+        [[1,2,2,4], [2,1], True],
+        [[2,1], [1,2,2,4], True],
+        [[1,2,2,4], [2,5], False],
+        [[2,6], [1,2,2,4], False],
+        [[1,2,2,4], [], True],
+        [[], [2,1], True],
+    )
+    @unpack
+    def test_is_subset(self, arr1, arr2, expect):
+        res = c.is_subset(arr1, arr2)
+        assert res == expect
+
+
+    @data(
+        [[1,2,2,4], [1,2], [1,2]],
+        [[5,1], [4,1], [1,]],
+        [[5,1], [4,2], []],
+    )
+    @unpack
+    def test_get_intersection(self, arr1, arr2, expect):
+        res = c.get_intersection(arr1,arr2)
+        assert res == expect
+
+    @data(
+        [['a','b','c','d','c','e','f'], 'c'],
+    )
+    @unpack
+    def test_detect_first_duplicate(self, arr, expect):
+        res = c.detect_first_duplicate(arr)
+        assert res == expect
+
+
+    @data(
+        ['minimum', 'n'],
+    )
+    @unpack
+    def test_detect_first_unique(self, input, expect):
+        res = c.detect_first_unique(input)
+        assert res == expect
+
+    @data(
+        ['the quick brown box jumps over a lazy dog', 'f'],
+    )
+    @unpack
+    def test_detect_missing_letters(self, input, expect):
+        res = c.detect_missing_letters(input)
+        assert res == expect
